a82e48ea17420d18b38d1270802ae796eb0fbfd7
mohammad <ekhlasimohammad345@gmail.com>
2025-03-20 18:05:48 +0100

    css changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,33 @@
-let name = 'Pokemon';
-console.log(name);
+let pokemons = [
+    {
+        thumbnail: 'bulbasaur.jpg',
+        id:'N°01' ,
+        name: 'bulbasaur',
+        type: ['plant', 'poison'],
+      },
+      {
+        thumbnail: 'charmander.jpg',
+        id:'N°02',
+        name: 'charmander',
+        type: ['fire'],
+      },
+      {
+        thumbnail: 'caterpie.jpg',
+        id:'N°03',
+        name: 'caterpie',
+        type: ['bug'],
+      },
+      {
+        thumbnail: 'kakuna.jpg',
+        id:'N°04',
+        name: 'kakuna',
+        type: ['bug', 'poison'],
+      },
+      {
+        thumbnail: 'rattata.jpg',
+        id:'N°05',
+        name: 'rattata',
+        type: ['plant'],
+      }
+];
+console.log(pokemons);
