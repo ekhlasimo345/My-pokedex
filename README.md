bfd53a98e778e39e0fa7a04633a98447c7c7f978
mohammad <ekhlasimohammad345@gmail.com>
2025-03-24 19:47:48 +0100

    new changes

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ to With an array, you should manipulate the DOM to show on the screen each one o
 //make a for loop for my array to follow the structure of html regularlly after each other.
 //make a DOM manipulation to connect with my html
 
+steps :
+1:creat a div element to add iimg element
+2: creat an image source
+3: add the img to the div
+4: add the div to the HTML file 
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,97 +1,99 @@
+
+
 let pokemons = 
 [
     {
-      thumbnail: '001',
+      thumbnail: '001.png',
       id:'N°01' ,
       name: 'bulbasaur',
       type: ['plant', 'poison'],
     },
     {
-      thumbnail: '002',
+      thumbnail: '002.png',
       id:'N°02',
-      name: 'charmander',
+      name: 'charmander.png',
       type: ['fire'],
     },
     {
-      thumbnail: '003',
+      thumbnail: '003.png',
       id:'N°03',
       name: 'caterpie',
       type: ['bug'],
     },
     {
-      thumbnail: '004',
+      thumbnail: '004.png',
       id:'N°04',
       name: 'kakuna',
       type: ['bug', 'poison'],
     },
     {
-      thumbnail: '005',
+      thumbnail: '005.png',
       id:'N°05',
       name: 'rattata',
       type: ['plant'],
     },
     {
-      thumbnail: '006',
+      thumbnail: '006.png',
       id:'N°06',
       name: 'pikachu',
       type: ['normal'],
     },
     {
-      thumbnail: '007',
+      thumbnail: '007.png',
       id:'N°07',
       name: 'vulpix',
       type: ['fire', 'plant'],
     },
     {
-      thumbnail: '008',
+      thumbnail: '008.png',
       id:'N°08',
       name: 'jigglypuff',
       type: ['normal', 'fairy'],
     },
     {
-      thumbnail: '009',
+      thumbnail: '009.png',
       id:'N°09',
       name: 'mew',
       type: ['phychic'],
     },
     {
-      thumbnail: '010',
+      thumbnail: '010.png',
       id:'N°10',
       name: 'ponyta',
       type: ['fire'],
     },
     {
-      thumbnail: '011',
+      thumbnail: '011.png',
       id:'N°11',
       name: 'cloyster',
       type: ['ice', 'water'],
     },
     {
-      thumbnail: '012',
+      thumbnail: '012.png',
       id:'N°12',
       name: 'groundor',
       type: ['ground'],
     },
     {
-      thumbnail: '013',
+      thumbnail: '013.png',
       id:'N°13',
       name: 'cubone',
       type: ['ground'],
     },
     {
-      thumbnail: '014',
+      thumbnail: '014.png',
       id:'N°14',
       name: 'tentacruel',
       type: ['poison', 'water'],
     },
     {
-      thumbnail: '015',
+      thumbnail: '015.png',
       id:'N°15',
       name: 'darkrai',
       type: ['dark'],
     },
     {
-      thumbnail: '016',
+      thumbnail: '016.png',
       id:'N°16',
       name: 'diglett',
       type: ['ground'],
@@ -99,3 +101,6 @@ let pokemons =
   ]  
 ;
 console.log(pokemons);
+
+
+const pokemonList = document.getElementsByClassName("poke-cards")
