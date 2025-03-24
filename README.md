9074595b0d4a4dfc2415083bbd74e943f203ee2d
mohammad <ekhlasimohammad345@gmail.com>
2025-03-24 16:58:17 +0100

    NEW CHANGES

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # My-pokedex
-trying the exercise
+
+to With an array, you should manipulate the DOM to show on the screen each one of the pokemons, following the structure of the HTML you previously created:
+
+//make a for loop for my array to follow the structure of html regularlly after each other.
+//make a DOM manipulation to connect with my html
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,33 +1,101 @@
-let pokemons = [
+let pokemons = 
+[
     {
-        thumbnail: 'bulbasaur.jpg',
-        id:'N°01' ,
-        name: 'bulbasaur',
-        type: ['plant', 'poison'],
-      },
-      {
-        thumbnail: 'charmander.jpg',
-        id:'N°02',
-        name: 'charmander',
-        type: ['fire'],
-      },
-      {
-        thumbnail: 'caterpie.jpg',
-        id:'N°03',
-        name: 'caterpie',
-        type: ['bug'],
-      },
-      {
-        thumbnail: 'kakuna.jpg',
-        id:'N°04',
-        name: 'kakuna',
-        type: ['bug', 'poison'],
-      },
-      {
-        thumbnail: 'rattata.jpg',
-        id:'N°05',
-        name: 'rattata',
-        type: ['plant'],
-      }
-];
+      thumbnail: '001',
+      id:'N°01' ,
+      name: 'bulbasaur',
+      type: ['plant', 'poison'],
+    },
+    {
+      thumbnail: '002',
+      id:'N°02',
+      name: 'charmander',
+      type: ['fire'],
+    },
+    {
+      thumbnail: '003',
+      id:'N°03',
+      name: 'caterpie',
+      type: ['bug'],
+    },
+    {
+      thumbnail: '004',
+      id:'N°04',
+      name: 'kakuna',
+      type: ['bug', 'poison'],
+    },
+    {
+      thumbnail: '005',
+      id:'N°05',
+      name: 'rattata',
+      type: ['plant'],
+    },
+    {
+      thumbnail: '006',
+      id:'N°06',
+      name: 'pikachu',
+      type: ['normal'],
+    },
+    {
+      thumbnail: '007',
+      id:'N°07',
+      name: 'vulpix',
+      type: ['fire', 'plant'],
+    },
+    {
+      thumbnail: '008',
+      id:'N°08',
+      name: 'jigglypuff',
+      type: ['normal', 'fairy'],
+    },
+    {
+      thumbnail: '009',
+      id:'N°09',
+      name: 'mew',
+      type: ['phychic'],
+    },
+    {
+      thumbnail: '010',
+      id:'N°10',
+      name: 'ponyta',
+      type: ['fire'],
+    },
+    {
+      thumbnail: '011',
+      id:'N°11',
+      name: 'cloyster',
+      type: ['ice', 'water'],
+    },
+    {
+      thumbnail: '012',
+      id:'N°12',
+      name: 'groundor',
+      type: ['ground'],
+    },
+    {
+      thumbnail: '013',
+      id:'N°13',
+      name: 'cubone',
+      type: ['ground'],
+    },
+    {
+      thumbnail: '014',
+      id:'N°14',
+      name: 'tentacruel',
+      type: ['poison', 'water'],
+    },
+    {
+      thumbnail: '015',
+      id:'N°15',
+      name: 'darkrai',
+      type: ['dark'],
+    },
+    {
+      thumbnail: '016',
+      id:'N°16',
+      name: 'diglett',
+      type: ['ground'],
+    }
+  ]  
+;
 console.log(pokemons);
